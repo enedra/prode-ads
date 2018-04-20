3d4d388191eb07c417c5e2d373410b1dc08e9c83
Mariano Córdoba <marianocordoba039@gmail.com>
2018-04-19 21:03:50 -0300

    Sample working test

--- src/test/java/com/eprode/AppTest.java
+++ src/test/java/com/eprode/AppTest.java
@@ -1,20 +1,36 @@
 package com.eprode;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setupStreams() {
+        System.setOut(new PrintStream(outContent));
     }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(System.out);
+    }
+
+    @Test
+    public void testHelloWorld() {
+        App.main(new String[0]);
+        assertEquals("Hello World!\n", outContent.toString());
+    }
+
 }
