dc637a008f8a60d211e03c3b54fb504e49f05135
TomasGuevara <renzotomasguevara@gmail.com>
2018-05-10 18:00:04 -0300

    list

--- src/main/java/com/eprode/models/EGame.java
+++ src/main/java/com/eprode/models/EGame.java
@@ -7,7 +7,79 @@ import org.javalite.activejdbc.annotations.Table;
 public class EGame extends Model {
 
     static {
-        validatePresenceOf("name", "description");
+        validatePresenceOf("name", "cod_game");
+    }
+
+}
+
+@Table("users")
+public class Users extends Model {
+
+    static {
+        validatePresenceOf("id_user", "admin");
+    }
+
+}
+
+@Table("fixture")
+public class Fixture extends Model {
+
+    static {
+        validatePresenceOf("id_fixture", "id_user", "cod_game");
+    }
+
+}
+
+@Table("team")
+public class Team extends Model {
+
+    static {
+        validatePresenceOf("id_team", "id_user", "name");
+    }
+
+}
+
+@Table("match")
+public class Match extends Model {
+
+    static {
+        validatePresenceOf("id_match","id_fixture", "id_user", "id_team" , "modalidad", "date");
+    }
+
+}
+
+@Table("prediction")
+public class Prediction extends Model {
+
+    static {
+        validatePresenceOf("id_prediction", "id_user", "id_match");
+    }
+
+}
+
+@Table("result")
+public class Result extends Model {
+
+    static {
+        validatePresenceOf("id_result", "id_match", "id_stats", "rTeam1", "rTeam2");
+    }
+
+}
+
+@Table("predictionResult")
+public class PredictionResult extends Model {
+
+    static {
+        validatePresenceOf("id_prediction", "id_result", "difPRT1", "difPRT2");
+    }
+
+}
+
+@Table("stats")
+public class Stats extends Model {
+
+    static {
+        validatePresenceOf("id_stats", "id_user");
     }
 
 }
