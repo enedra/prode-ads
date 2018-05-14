8d81493258782e711e84e256df311a1c5137b929
TomasGuevara <renzotomasguevara@gmail.com>
2018-05-14 11:27:24 -0300

    reformulado

--- src/main/java/com/eprode/models/EGame.java
+++ src/main/java/com/eprode/models/EGame.java
@@ -3,6 +3,9 @@ package com.eprode.models;
 import org.javalite.activejdbc.Model;
 import org.javalite.activejdbc.annotations.Table;
 
+/*
+*/
+
 @Table("egames")
 public class EGame extends Model {
 
